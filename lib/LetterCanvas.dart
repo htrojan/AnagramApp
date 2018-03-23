aa2eae7dc7daacc71e18e3f0fdd2ff6ad0011436
lobbi44 <hendyGA@posteo.de>
2018-03-23 19:39:38 +0100

    Splitting State of LetterCanvas from buildMethod

--- lib/LetterCanvas.dart
+++ lib/LetterCanvas.dart
@@ -16,6 +16,7 @@ class LetterCanvas extends StatefulWidget {
 
   final List<int> _letters;
   final LetterCallback _callback;
+  final int _rowCount = 3;
 
   @override
   _LetterCanvasState createState() => new _LetterCanvasState();
@@ -28,52 +29,63 @@ class _LetterCanvasState extends State<LetterCanvas> {
   final int _maxRnd = 3;
   final List<Color> _rndColors = [Colors.yellow, Colors.amber, Colors.red];
   Set<int> _indexesDeactivated = new Set();
-  
-  List<Widget> _generateRandomLayout(final int rowCount) {
-    
-    final int maxUsed = widget._letters.length;
-    final int maxPerRow = (maxUsed / rowCount).round() + 1;
-    int alreadyUsed = 0;
-    
 
-    return new List.generate(rowCount, (index) {
-      int num = (index == rowCount - 1) ? (maxUsed - alreadyUsed) : (_rnd
+  List<int> _boxesPerRow = new List();
+
+  @override
+  void initState(){
+    super.initState();
+    final int maxUsed = widget._letters.length;
+    final int maxPerRow = (maxUsed / widget._rowCount).round() + 1;
+    int alreadyUsed = 0;
+
+    _boxesPerRow = new List.generate(widget._rowCount, (index){
+      int num = (index == widget._rowCount) ? (maxUsed - alreadyUsed) : (_rnd
           .nextInt(maxPerRow) + 1);
       if (num > maxUsed - alreadyUsed) {
         num = 0;
       }
-      int currentIndex = alreadyUsed -1;
-      var row = new Expanded(
-        child: new Row(
 
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: widget._letters.skip(alreadyUsed).take(num).map((rune) {
-            currentIndex++;
-            return new GestureDetector(
-              onTap: (){
-                _indexesDeactivated.add(currentIndex);
-                debugPrint(currentIndex.toString() + ' was added to pressed buttons');
-                widget._callback(rune);
-              },
-              child: new LetterBox(
-                _rndColors[_rnd.nextInt(_maxRnd)],
-                new String.fromCharCode(rune).toUpperCase(),
-              ),
-            );
-          }).toList(),
-        ),
-      );
-
-      alreadyUsed += num;
-      return row;
+      return num;
     });
+  }
+
+  List<Widget> _generateRandomLayout() {
+    int currentIndex = 0;
+
+    return _boxesPerRow.map((rowCount){
+
+      return new Expanded(
+          child: new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: widget._letters.skip(currentIndex).take(rowCount).map((rune){
+              currentIndex++; //A 'custom' made index to keep track of each letter
+              //As it is incremented before the return statement, all variables inside the return statement
+              //have to use currentIndex-1
+              return new GestureDetector(
+                onTap: () {
+                  _indexesDeactivated.add(currentIndex-1);
+                  debugPrint((currentIndex-1).toString() +
+                      ' was added to pressed buttons');
+                  widget._callback(rune);
+                },
+                child: new LetterBox(
+                  _rndColors[_rnd.nextInt(_maxRnd)],
+                  new String.fromCharCode(rune).toUpperCase(),
+                ),
+              );
+
+            }).toList(),
+          )
+      );
+    }).toList();
   }
 
   @override
   Widget build(BuildContext context) {
     return new Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: _generateRandomLayout(3)
+        children: _generateRandomLayout()
     );
   }
 }
